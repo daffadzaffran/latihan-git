e46246f85689ea78cc1af436fd8fd704879801d0
daffadzaffran <90884124+daffadzaffran@users.noreply.github.com>
2021-10-01 16:13:33 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Saya Daffa Dzaffran
+        Saya tinggal di bintaro
+        keseharian saya bermain
+        musik";
+    }
+}
